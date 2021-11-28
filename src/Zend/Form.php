0c7df7c5e5919e89abf388dede5eef6f230bdd23
Jay Klehr <jay@diablomedia.com>
2021-11-28 01:21:21 -0700

    small fixes for zf1 tests

--- src/Zend/Form.php
+++ src/Zend/Form.php
@@ -3260,6 +3260,7 @@ class Zend_Form implements Iterator, Countable, Zend_Validate_Interface
      * @throws Zend_Form_Exception
      * @return Zend_Form_Element|Zend_Form_DisplayGroup|Zend_Form
      */
+    #[\ReturnTypeWillChange]
     public function current()
     {
         $this->_sort();
@@ -3282,6 +3283,7 @@ class Zend_Form implements Iterator, Countable, Zend_Validate_Interface
      *
      * @return string
      */
+    #[\ReturnTypeWillChange]
     public function key()
     {
         $this->_sort();
